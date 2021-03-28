a8c116df8238718377913c6a68c656747cbe461f
gitbranch01 <a598919335@163.com>
2021-03-28 22:47:59 +0800

    v2.1.2
    修复：
        修复了无法读取密码时会错误启动的问题。
        修复了无法读取密码时会错误地启动一瞬间的问题。
        修复了输入密码启动后启动页不会关闭的问题。
        修复了关于页中MSVC版本显示错误的问题。

--- commoninclude.h
+++ commoninclude.h
@@ -7,13 +7,13 @@
 #define TO_STR1(I1) #I1
 #define TO_STR2(I2) TO_STR1(I2)
 
-#define ABOUT_VERSION "2.1.1"
+#define ABOUT_VERSION "2.1.2"
 #define ABOUT_TIME "2021-3-28"
 #define ABOUT_BASE_QT "Qt " QT_VERSION_STR
 
 
 #ifdef Q_CC_MSVC
-#   define ABOUT_BASE_COMPILER "MSVC " TO_STR2(Q_CC_MSVC)
+#   define ABOUT_BASE_COMPILER "MSVC " TO_STR2(_MSVC_LANG)
 #elif defined(Q_CC_MINGW)
 #   define ABOUT_BASE_COMPILER "MinGW " TO_STR2(__GNUC__) "." TO_STR2(__GNUC_MINOR__) "." TO_STR2(__GNUC_PATCHLEVEL__)
 #elif defined(__GNUC__)
